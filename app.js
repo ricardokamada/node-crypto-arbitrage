cc067190b8d577737fe9439493763e239885c572
ricardokamada <ricardokamada@gmail.com>
2022-05-25 08:48:03 -0300

    Refactor and new app node-binace

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
+require('dotenv').config()
 const axios = require('axios');
-const QUOTE = 'BUSD';
+const QUOTE = 'USDT';
 const AMOUNT = 10;
 const PROFITABILITY = 1.00225;
 
-const Websocket = require('ws');
-const ws = new Websocket("wss://stream.binance.com:9443/ws/!bookTicker");
+const Binance = require('node-binance-api');
+const binance = new Binance({
+    APIKEY: "process.env.APIKEY",
+    APISECRET: "process.env.APISECRET"
+});
 
 const BOOK = {};
-
-ws.onmessage = async (event) => {
-    const obj = JSON.parse(event.data);
-    BOOK[obj.s] = {
-        ask: parseFloat(obj.a),
-        bid: parseFloat(obj.b)
+binance.websockets.bookTickers((res) => {
+    BOOK[res.symbol] = {
+        ask: parseFloat(res.bestAsk),
+        bid: parseFloat(res.bestBid)
     };
-}
+}) 
+
+
 
 async function exchangeInfo(){
-    const response = await axios.get("https://api.binance.com/api/v3/exchangeInfo");
-    const symbols = response.data.symbols.filter(s => s.status === 'TRADING'); 
-    return symbols.map(s => {
-        return {
-            symbol: s.symbol,
-            base: s.baseAsset,
-            quote: s.quoteAsset
-        }
-    })   
+    let allSymbols = await binance.exchangeInfo()
+    .then(response => {
+        const symbols = response.symbols.filter(s => s.status === 'TRADING');
+        return symbols.map(s => {
+            return {
+                symbol: s.symbol,
+                base: s.baseAsset,
+                quote: s.quoteAsset
+            }
+        })
+    })
+    return allSymbols;
 }
 
 function getBuyBuySell(buySymbols, allSymbols){
@@ -48,27 +55,6 @@ function getBuyBuySell(buySymbols, allSymbols){
     return buyBuySell;    
 }
 
-function getBuySellSell(buySymbols, allSymbols){
-    const buySellSell = [];
-
-    for (let i=0; i < buySymbols.length; i++){
-        const buy1 = buySymbols[i];
-
-        const right = allSymbols.filter(s => s.base === buy1.base && s.quote !== buy1.quote);
-
-        for(let j=0; j< right.length; j++){
-            const sell1 = right[j];
-
-            const sell2 = allSymbols.find(s => s.base === sell1.quote && s.quote === buy1.quote);
-            if(!sell2) continue;
-
-            buySellSell.push({buy1, sell1, sell2});
-        }
-    }
-    return buySellSell;    
-    
-}
-
 function processBuyBuySell(buyBuySell){
     for(let i=0; i< buyBuySell.length; i++ ){
         const candidate = buyBuySell[i];
@@ -87,13 +73,14 @@ function processBuyBuySell(buyBuySell){
 
         const crossRate = (1/priceBuy1) * (1/priceBuy2) * priceSell1;
 
+        
         if(crossRate > PROFITABILITY){
             console.log(`Oportunidade em ${candidate.buy1.symbol} > ${candidate.buy2.symbol} > ${candidate.sell1.symbol} = ${crossRate} `);
         }
     }
 }
 
-async function process(){
+async function process() {
     const allSymbols = await exchangeInfo();
 
     const buySymbols = allSymbols.filter(s => s.quote === QUOTE);
@@ -101,16 +88,18 @@ async function process(){
     const buyBuySell = getBuyBuySell(buySymbols, allSymbols);
     console.log("Existem " + buyBuySell.length + " Pares BBS");
 
-    const buySellSell = getBuySellSell(buySymbols, allSymbols);
-    console.log("Existem " + buySellSell.length + " Pares BSS");
-
     setInterval(() => {
         console.log(new Date());
         processBuyBuySell(buyBuySell);
     },3000)
-   
 
-   
 }
 
 process();
+
+
+
+
+
+
+
